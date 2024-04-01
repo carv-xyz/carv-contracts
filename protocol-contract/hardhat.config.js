fbfeca81b70b1ec3eb2839e1a30669451837898e
xuxinlai2002 <xxl@local.com>
2024-04-01 21:56:49 +0800

    delete the infro key info

--- protocol-contract/hardhat.config.js
+++ protocol-contract/hardhat.config.js
@@ -13,12 +13,9 @@ module.exports = {
 
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/F1X0FmmZ95T61-D1LldN5QYam9D0amLn`,
-      accounts: [
-        "0xa6392433fe30f2bf8564228240eddd41c7ad12ab5332438254054896790ceebe",
-        "0xbb568983fe7440b0197b6c990f4249fe83a07d03c7433d82c9d49f8f1e0fea3e",
-        "0xadc22517f2de0093429e5365b042da0ec9299353943db0f0cc104743c69104cf",
-        "0xada29a473e2b777403e7d2dc3876c5be03ca6b60d97e37e9bd335b1ce05a2680",
-        "0xd1179d8889a1c7697d753528571b05d6d95e687ebfa12f03741f7f3d5909870b"
+      accounts: 
+      [
+        process.env.privateKey
       ]
     },
     
@@ -33,11 +30,9 @@ module.exports = {
 
     hardhat:{
       chainId:100,
-      accounts: [
-        {privateKey:"0xa6392433fe30f2bf8564228240eddd41c7ad12ab5332438254054896790ceebe",balance:"10000000000000000000000"},
-        {privateKey:"0xf143b04240e065984bc0507eb1583234643d64c948e1e0ae2ed4abf7d7aed06a",balance:"10000000000000000000000"},
-        {privateKey:"0x49b9dd4e00cb10e691abaa1de4047f9c9d98b72b9ce43e1e12959b22f56a0289",balance:"10000000000000000000000"},
-        {privateKey:"0xada29a473e2b777403e7d2dc3876c5be03ca6b60d97e37e9bd335b1ce05a2680",balance:"10000000000000000000000"},
+      accounts:
+      [
+        process.env.privateKey
       ],
       blockGasLimit: 8000000
     },
@@ -46,12 +41,9 @@ module.exports = {
     baseTest:
     {
       url: `https://goerli.base.org`,
-      accounts: [
-        "0xa6392433fe30f2bf8564228240eddd41c7ad12ab5332438254054896790ceebe",
-        "0xbb568983fe7440b0197b6c990f4249fe83a07d03c7433d82c9d49f8f1e0fea3e",
-        "0xadc22517f2de0093429e5365b042da0ec9299353943db0f0cc104743c69104cf",
-        "0xada29a473e2b777403e7d2dc3876c5be03ca6b60d97e37e9bd335b1ce05a2680",
-        "0xd1179d8889a1c7697d753528571b05d6d95e687ebfa12f03741f7f3d5909870b"
+      accounts: 
+      [
+        process.env.privateKey
       ]
     },
 
@@ -59,12 +51,9 @@ module.exports = {
     opTest:
     {
       url: `https://goerli.optimism.io`,
-      accounts: [
-        "0xa6392433fe30f2bf8564228240eddd41c7ad12ab5332438254054896790ceebe",
-        "0xbb568983fe7440b0197b6c990f4249fe83a07d03c7433d82c9d49f8f1e0fea3e",
-        "0xadc22517f2de0093429e5365b042da0ec9299353943db0f0cc104743c69104cf",
-        "0xada29a473e2b777403e7d2dc3876c5be03ca6b60d97e37e9bd335b1ce05a2680",
-        "0xd1179d8889a1c7697d753528571b05d6d95e687ebfa12f03741f7f3d5909870b"
+      accounts: 
+      [
+        process.env.privateKey
       ]
     },
 
@@ -73,12 +62,9 @@ module.exports = {
     {
       url: `https://opbnb-testnet-rpc.bnbchain.org`,
       chainId: 5611, // Replace with the correct chainId for the "opbnb" network
-      accounts: [
-        "0xa6392433fe30f2bf8564228240eddd41c7ad12ab5332438254054896790ceebe",
-        "0xbb568983fe7440b0197b6c990f4249fe83a07d03c7433d82c9d49f8f1e0fea3e",
-        "0xadc22517f2de0093429e5365b042da0ec9299353943db0f0cc104743c69104cf",
-        "0xada29a473e2b777403e7d2dc3876c5be03ca6b60d97e37e9bd335b1ce05a2680",
-        "0xd1179d8889a1c7697d753528571b05d6d95e687ebfa12f03741f7f3d5909870b"
+      accounts: 
+      [
+        process.env.privateKey
       ],
       gasPrice: 20000000000,
       allowUnlimitedContractSize: true,
@@ -86,19 +72,16 @@ module.exports = {
 
     arbitrumGoerli: {
       url: `https://goerli-rollup.arbitrum.io/rpc/`,
-      accounts: [
-        "0xa6392433fe30f2bf8564228240eddd41c7ad12ab5332438254054896790ceebe",
-        "0xbb568983fe7440b0197b6c990f4249fe83a07d03c7433d82c9d49f8f1e0fea3e",
-        "0xadc22517f2de0093429e5365b042da0ec9299353943db0f0cc104743c69104cf",
-        "0xada29a473e2b777403e7d2dc3876c5be03ca6b60d97e37e9bd335b1ce05a2680",
-        "0xd1179d8889a1c7697d753528571b05d6d95e687ebfa12f03741f7f3d5909870b"
+      accounts: 
+      [
+        process.env.privateKey
       ]
     },    
 
     sepolia: {
-      url: `https://sepolia.infura.io/v3/7e31d49d7c8a48f4a4539aff9da768e7`,
+      url: `https://sepolia.infura.io/v3/${process.env.apiKey}`,
       accounts: [
-        "0xbb568983fe7440b0197b6c990f4249fe83a07d03c7433d82c9d49f8f1e0fea3e"
+        process.env.privateKey
       ]
     },
 
@@ -134,7 +117,7 @@ module.exports = {
   },
 
   etherscan: {
-    apiKey: "A9TYNSB1RZC713ZWRW44R4WY1CRYD9BDJ4"
+    apiKey: process.env.apiKey
   },
   
 }
